12fbe15150c19cb38a80b4e4818dc60c33f56f44
teseo <sol@javiermellado.com>
2019-02-07 03:03:34 +0200

    calculate winners

--- src/Components/Board/Board.component.tsx
+++ src/Components/Board/Board.component.tsx
@@ -18,18 +18,26 @@ const Status = styled.div`
 type MyProps = {};
 type MyState = {
   squares: string[];
+  xIsNext: boolean;
 };
 export default class Board extends React.Component<MyProps, MyState> {
   constructor(props: MyProps) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true
     };
   }
   handleClick(i: number) {
     const squares = this.state.squares.slice();
-    squares[i] = "X";
-    this.setState({ squares: squares });
+    if (this.calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    });
   }
   renderSquare(i: number) {
     return (
@@ -39,10 +47,37 @@ export default class Board extends React.Component<MyProps, MyState> {
       />
     );
   }
-
+  calculateWinner(squares: string[]) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
   render() {
-    const status = "Next player: X";
-
+    const winner = this.calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
     return (
       <BoardContainer>
         <Status>{status}</Status>
